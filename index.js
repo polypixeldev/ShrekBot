5e4d7a7b4640ae98133b5b6363ad12dc754dee6c
PolyPixel <79737178+Poly-Pixel@users.noreply.github.com>
2021-09-01 21:59:31 +0000

    Add user inputs & responses to payload

--- index.js
+++ index.js
@@ -3,89 +3,98 @@ const Discord = require('discord.js');
 // node-fetch for making HTTP requests
 const fetch = require('node-fetch');
 
-//keepalive function
 const keepalive = require('./keepalive.js')();
 
-// initialize client
-const client = new Discord.Client({intents: ["GUILD_MESSAGES", "GUILDS"]});
-// my model URL
-const MODEL = 'shrek-medium'
+const client = new Discord.Client({ intents: ["GUILD_MESSAGES", "GUILDS"] });
+
+const MODEL = 'shrek-medium-full'
 
 const API_URL = `https://api-inference.huggingface.co/models/Poly-Pixel/${MODEL}`;
 
-// log out some info
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// when the bot receives a message
-// need async message because we are making HTTP requests
 let loading = false;
+let userResponses = {}
+let userInputs = {}
 client.on('message', async message => {
-    // ignore messages from the bot itself
-    if (message.author.bot || message.author.system || message.cleanContent === '') {
+  if (message.author.bot || message.author.system || message.cleanContent === '') {
+    return;
+  }
+
+  const payload = {
+    inputs: {
+      text: message.cleanContent,
+      generated_responses: userResponses[message.author.id] ? userResponses[message.author.id] : [],
+      past_user_inputs: userInputs[message.author.id] ? userInputs[message.author.id] : []
+    },
+    options: {
+      use_cache: false
+    }
+  };
+
+  const headers = {
+    'Authorization': 'Bearer ' + process.env.HUGGINGFACE_TOKEN
+  };
+
+
+  message.channel.startTyping();
+
+  const response = await fetch(API_URL, {
+    method: 'post',
+    body: JSON.stringify(payload),
+    headers: headers
+  });
+
+  const data = await response.json();
+  let botResponse = '';
+  if (data.hasOwnProperty('generated_text')) {
+    botResponse = data.generated_text;
+  } else if (data.hasOwnProperty('error')) { // error condition
+    console.log(data)
+    let embed;
+    if (data.error === `Model Poly-Pixel/${MODEL} is currently loading`) {
+      if (loading === false) {
+        embed = new Discord.MessageEmbed()
+          .setColor(0x00FF00)
+          .setDescription(`Shrek will be at the swamp soon... \n \n Est. Time left: \`${data.estimated_time}\``)
+
+        message.react('⏲')
+        loading = true;
+      } else {
+        message.react('⏲')
+        message.channel.stopTyping()
         return;
-    }
-    // form the payload
-    const payload = {
-        inputs: {
-            text: message.cleanContent
-        }
-    };
-    // form the request headers with Hugging Face API key
-    const headers = {
-        'Authorization': 'Bearer ' + process.env.HUGGINGFACE_TOKEN
-    };
-    
-    // set status to typing
-    message.channel.startTyping();
-    // query the server
-    const response = await fetch(API_URL, {
-        method: 'post',
-        body: JSON.stringify(payload),
-        headers: headers
-    });
-    const data = await response.json();
-    let botResponse = '';
-    if (data.hasOwnProperty('generated_text')) {
-        botResponse = data.generated_text;
-    } else if (data.hasOwnProperty('error')) { // error condition
-        console.log(data)
-        let embed;
-        if(data.error === `Model Poly-Pixel/${MODEL} is currently loading`){
-          if(loading === false){
-            embed = new Discord.MessageEmbed()
-            .setColor(0x00FF00)
-            .setDescription(`Shrek will be at the swamp soon... \n \n Est. Time left: \`${data.estimated_time}\``) 
-            
-            message.react('⏲')
-            loading = true;
-          } else {
-            message.react('⏲')
-            return;
-          }
-        } else {
-          embed = new Discord.MessageEmbed()
-          .setColor(0xFF0000)
-          .setDescription(data.error)
-        }
-        botResponse = embed;
-    }
-
-    if(botResponse.length > 2000){
-      botResponse = botResponse.slice(0, 1986)
-      botResponse = botResponse + "`(truncated)`"
-    }
-
-    // send message to channel as a reply
-    message.channel.stopTyping()
-    if(botResponse.trim){
-      if(botResponse.trim().length > 0){
-        message.channel.send(botResponse);
       }
     } else {
-      message.channel.send(botResponse);
+      embed = new Discord.MessageEmbed()
+        .setColor(0xFF0000)
+        .setDescription(data.error)
     }
+    botResponse = embed;
+  }
+
+  if (botResponse.length > 2000) {
+    botResponse = botResponse.slice(0, 1986)
+    botResponse = botResponse + "`(truncated)`"
+  }
+
+  // send message to channel as a reply
+  message.channel.stopTyping()
+  if (botResponse.trim) {
+    if (botResponse.trim().length > 0){
+      message.channel.send(botResponse);
+      if(!userInputs[message.author.id] || !userResponses[message.author.id]) {
+         userInputs[message.author.id] = [];
+         userResponses[message.author.id] = [];
+      }
+      userInputs[message.author.id].push(message.cleanContent)
+      userResponses[message.author.id].push(botResponse)
+    }
+  } else {
+    message.channel.send(botResponse);
+  }
 })
 
 client.login(process.env.DISCORD_TOKEN);
